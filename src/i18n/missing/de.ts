641f2f02af9f6a967ccb1baa729ee515480c4005
mpatfield <martin@patfield.com>
2026-02-27 12:55:43 -0800

    Update missing

---
+++ src/i18n/missing/de.ts
@@ -0,0 +1,22 @@
+const de = {
+
+  config: {
+
+    description: {
+      useNotifications: 'If true, an unread usage alert notification will be considered a leak',
+    },
+
+    title: {
+      useNotifications: 'Use Unread Usage Notifications',
+    },
+  },
+
+  startup: {
+    welcome: [
+      'Want to see this plugin in your own language? Please visit https://github.com/mpatfield/homebridge-flume/issues/107',
+    ],
+  },
+
+};
+
+export default de;
